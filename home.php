1a5050d2dff5d302daa0f51f527c5551c6383c6c
Stefano Cremona <stefano.cremona@gmail.com>
2019-01-07 22:48:01 +0000

    Add basic home page

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!-- saved from url=(0072)http://stuiis.cms.gre.ac.uk/ha07/Responsive/Breakpoint/w3c-3b.html#about -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./res/responsive.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>EmotiGre - Greenwich Emotions Analyzer</h1>
+</div>
+
+<ul class="topnav">
+  <!-- <li><a class="active" href="#home">Home</a></li> -->
+  <li><a href="http://stuiis.cms.gre.ac.uk/ha07/Responsive/Breakpoint/w3c-3b.html#home">Home</a></li>
+  <li><a href="http://stuiis.cms.gre.ac.uk/ha07/Responsive/Breakpoint/w3c-3b.html#news">Users</a></li>
+  <li><a href="http://stuiis.cms.gre.ac.uk/ha07/Responsive/Breakpoint/w3c-3b.html#contact">Profile</a></li>
+  <li><a href="http://stuiis.cms.gre.ac.uk/ha07/Responsive/Breakpoint/w3c-3b.html#about">Admin</a></li>
+</ul>
+
+<div class="row">
+
+<div class="col-3 col-m-3 menu">
+  <ul>
+    <li>All Tweets</li>
+    <li>Positive</li>
+    <li>Negative</li>
+    <li>Neutral</li>
+  </ul>
+</div>
+
+<div class="col-6 col-m-9">
+  <h1>The Tweets</h1>
+  <p>Bla bla bla</p>
+</div>
+
+<div class="col-3 col-m-12">
+  <div class="aside">
+    <h2>What?</h2>
+    <p>Chania is a city on the island of Crete.</p>
+    <h2>Where?</h2>
+    <p>Crete is a Greek island in the Mediterranean Sea.</p>
+    <h2>How?</h2>
+    <p>You can reach Chania airport from all over Europe.</p>
+  </div>
+</div>
+
+</div>
+
+<div class="footer">
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+</div>
+
+
+
+
+</body></html>
