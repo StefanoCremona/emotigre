b2ebf511c33afd6ee103f670d8e44cc247e70522
StefanoCremona <stefano.cremona@gmail.com>
2019-01-08 13:49:45 +0000

    Dinamically populate the tweets

--- res/home.js
+++ res/home.js
@@ -3,65 +3,43 @@ const USERPAGE = 1;
 const PROFILEPAGE = 2;
 const ADMINPAGE = 3;
 
+const tweets = [{
+        screenName: "CremonaStefano",
+        content: "Non ce la faccio piuuuuuuuuu!! Quando finisce?"
+    },{
+        screenName: "La Patata",
+        content: "Sono fritta?"
+    },{
+        screenName: "Massimo",
+        content: "Mi sono perso i nipotini"
+    },
+];
+
 function populateMainPage(page) {
     let innerHTML = '<div id="usersMainPage" class="mainPage"><div class="mainPageTitle">No page found</div></div>';
     switch (page) {
         case USERPAGE:
-            innerHTML = '<div id="usersMainPage" class="mainPage"><div class="mainPageTitle">User Page</div></div>';
+            innerHTML = '<div id="usersMainPage" class="mainPage"><div class="mainPageTitle">The Users</div></div>';
             break;
         case PROFILEPAGE:
-            innerHTML = '<div id="usersMainPage" class="mainPage"><div class="mainPageTitle">Profile Page</div></div>';
+            innerHTML = '<div id="usersMainPage" class="mainPage"><div class="mainPageTitle">Edit Your profile</div></div>';
             break;
         case ADMINPAGE:
-            innerHTML = '<div id="usersMainPage" class="mainPage"><div class="mainPageTitle">Admin Page</div></div>';
+            innerHTML = '<div id="usersMainPage" class="mainPage"><div class="mainPageTitle">Set the keywords</div></div>';
             break;
         case TWEETPAGE:
         default:
             innerHTML = '<div id="tweetsMainPage" class="mainPage">' +
-            '<div class="mainPageTitle">The Tweets</div>' +
-            '<div id="avatarContainer" class="horizontal centeredV padded">' +
-              '<img class="icon" src=\'./res/tweetIcon.png\' />' +
-              '<div>' +
-                '<div class="tweetAuthor" >Stefano Cremona</div>' +
-                '<div class="tweetContent" >Non ce la faccio piuuuuuuuuu!! Quando finisce?</div>' +
-              '</div>' +
-            '</div>' +
-            '<div id="avatarContainer" class="horizontal centeredV padded">' +
-              '<img class="icon" src=\'./res/tweetIcon.png\' />' +
-              '<div>' +
-                '<div class="tweetAuthor" >Stefano Cremona</div>' +
-                '<div class="tweetContent" >Non ce la faccio piuuuuuuuuu!! Quando finisce?</div>' +
-                '</div>' +
-            '</div>' +
-            '<div id="avatarContainer" class="horizontal centeredV padded">' +
-              '<img class="icon" src=\'./res/tweetIcon.png\' />' +
-              '<div>' +
-                '<div class="tweetAuthor" >Stefano Cremona</div>' +
-                '<div class="tweetContent" >Non ce la faccio piuuuuuuuuu!! Quando finisce?</div>' +
-              '</div>' +
-            '</div>' +
-            '<div id="avatarContainer" class="horizontal centeredV padded">' +
-              '<img class="icon" src=\'./res/tweetIcon.png\' />' +
-              '<div>' +
-                '<div class="tweetAuthor" >Stefano Cremona</div>' +
-                '<div class="tweetContent" >Non ce la faccio piuuuuuuuuu!! Quando finisce?</div>' +
-              '</div>' +
-            '</div>' +
-            '<div id="avatarContainer" class="horizontal centeredV padded">' +
-              '<img class="icon" src=\'./res/tweetIcon.png\' />' +
-              '<div>' +
-                '<div class="tweetAuthor" >Stefano Cremona</div>' +
-                '<div class="tweetContent" >Non ce la faccio piuuuuuuuuu!! Quando finisce?</div>' +
-              '</div>' +
-            '</div>' +
-            '<div id="avatarContainer" class="horizontal centeredV padded">' +
-              '<img class="icon" src=\'./res/tweetIcon.png\' />' +
-              '<div>' +
-                '<div class="tweetAuthor" >Stefano Cremona</div>' +
-                '<div class="tweetContent" >Non ce la faccio piuuuuuuuuu!! Quando finisce?</div>' +
-              '</div>' +
-            '</div>' +
-          '</div>';
+            '<div class="mainPageTitle">The Tweets</div>';
+            tweets.forEach(element => {
+                innerHTML += '<div id="avatarContainer" class="horizontal centeredV padded">' +
+                                '<img class="icon" src=\'./res/tweetIcon.png\' />' +
+                                '<div>' +
+                                '<div class="tweetAuthor" >'+element.screenName+'</div>' +
+                                '<div class="tweetContent" >'+element.content+'</div>' +
+                                '</div>' +
+                            '</div>';
+            });
             break;
     }
     document.getElementById("mainPage").innerHTML = innerHTML;
